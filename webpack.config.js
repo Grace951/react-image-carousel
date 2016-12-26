ecc203b60dd64a8a9de8e8b5afa1950b6e9fb6c1
Grace951 <learningg@gmail.com>
2016-12-26 15:07:17 +0800

    try to fixed react refs must have owner: https://github.com/facebook/react/issues/6343

--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,7 @@ var config = {
     'react': { commonjs: 'react', commonjs2: 'react', amd: 'react', root: 'React' },
     'react-dom': { commonjs: 'react-dom', commonjs2: 'react-dom', amd: 'react-dom', root: 'ReactDOM' },
     'react-addons-css-transition-group': { commonjs: 'react-addons-css-transition-group', commonjs2: 'react-addons-css-transition-group', amd: 'react-addons-css-transition-group', root: ['React','addons','CSSTransitionGroup'] }
-  },  
+  }
 };
 
 module.exports = config;
